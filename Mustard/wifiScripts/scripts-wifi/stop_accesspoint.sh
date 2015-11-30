44a28a1265cd76ac3364b2bc3c4092d53184c0c4
Amadeus <github@amadeus.me.uk>
2015-11-30 13:33:45 -0500

    Update stop_accesspoint.sh

--- Mustard/wifiScripts/scripts-wifi/stop_accesspoint.sh
+++ Mustard/wifiScripts/scripts-wifi/stop_accesspoint.sh
@@ -11,14 +11,11 @@
 ######################################################################################
 ######################################################################################
 
-
-# Kill the access point - this needs maturing to kill all monX points
-airmon-ng stop wlan1mon
-
 # Stop the dhcp server
 service isc-dhcp-server stop
 
-# Kill all instances of airbase
-kill -9 $(ps -ef | grep -i airbase | awk '{print $2}')
+# Kill all instances of airbase-ng
+killall airbase-ng
 
+# Stop Mustard
 kill -9 $(ps aux | grep Mustard | grep -v grep | awk '{print $2}')
